aeedd3001c0defee42a2d19ac9b11ac338b0e509
Jeffrey Jaca <jeffreyjjaca@gmail.com>
2020-10-18 16:24:52 -0400

    added DAO interface methods

--- app/src/main/java/sheridan/jaca/assignment2/database/GameScoreDao.kt
+++ app/src/main/java/sheridan/jaca/assignment2/database/GameScoreDao.kt
@@ -1,4 +1,21 @@
 package sheridan.jaca.assignment2.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface GameScoreDao {
+    @Insert
+    suspend fun insert(score: GameScore):Long
+
+    @Query("SELECT * FROM scores WHERE id=:key")
+    fun get(key:Long) : LiveData<GameScore>
+
+    @Query("SELECT * FROM scores ORDER BY id")
+    fun getAll(): LiveData<List<GameScore>>
+
+    @Query("DELETE FROM scores")
+    suspend fun deleteAll()
 }
